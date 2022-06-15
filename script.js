609ba0bc4c1e590f68f12be390e4987341ebc381
AlexEgupov <alex.egupov7@gmail.com>
2022-06-15 22:19:10 +0300

    feat script.js lesson15

--- script.js
+++ script.js
@@ -11,19 +11,41 @@ const DomElement = function (selector) {
     this.fontSize = '20px';
 };
 
+// DomElement.prototype.createElement = function () {
+//     if (this.selector[0] == '.') {
+//         const addClass = this.selector.slice(1);
+
+//         body.insertAdjacentHTML('beforeend',
+//             `<div class='${addClass}' style='height: ${this.height}; width: ${this.width};
+//         background: ${this.bg}; font-size: ${this.fontSize}'>Я див</div>`);
+
+//     } else if (this.selector[0] == '#') {
+//         const addId = this.selector.slice(1);
+//         body.insertAdjacentHTML('beforeend',
+//             `<p id='${addId}' style='height: ${this.height}; width: ${this.width};
+//         background: ${this.bg}; font-size: ${this.fontSize}'>А я параграф</p>`);
+//     }
+// };
+
 DomElement.prototype.createElement = function () {
     if (this.selector[0] == '.') {
         const addClass = this.selector.slice(1);
 
-        body.insertAdjacentHTML('beforeend',
-            `<div class='${addClass}' style='height: ${this.height}; width: ${this.width}; 
-        background: ${this.bg}; font-size: ${this.fontSize}'>Я див</div>`);
+        const newDiv = document.createElement('div');
+        newDiv.className = addClass;
+        newDiv.textContent = 'Я див';
+        newDiv.style.cssText = `height: ${this.height}; width: ${this.width}; 
+        background: ${this.bg}; font-size: ${this.fontSize};`;
+        document.body.append(newDiv);
 
     } else if (this.selector[0] == '#') {
         const addId = this.selector.slice(1);
-        body.insertAdjacentHTML('beforeend',
-            `<p id='${addId}' style='height: ${this.height}; width: ${this.width}; 
-        background: ${this.bg}; font-size: ${this.fontSize}'>А я параграф</p>`);
+        const newP = document.createElement('p');
+        newP.className = addId;
+        newP.textContent = 'А я параграф';
+        newP.style.cssText = `height: ${this.height}; width: ${this.width}; 
+        background: ${this.bg}; font-size: ${this.fontSize};`;
+        document.body.append(newP);
     }
 };
 
